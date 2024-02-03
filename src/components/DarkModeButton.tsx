1f62370ca0ad8da4ca2621d0d36edfbf327ab96f
Aimar Anand <aimaranand02@gmail.com>
2024-02-03 14:14:27 +0800

    feat(darkmoode): add auto prefer color scheme

--- src/components/DarkModeButton.tsx
+++ src/components/DarkModeButton.tsx
@@ -1,8 +1,22 @@
 "use client";
 import DarkMode from "@/assets/darkmode.svg";
 import Image from "next/image";
+import { useEffect } from "react";
 
 function DarkModeButton() {
+  // useEffect for prefer color scheme
+  useEffect(() => {
+    if (
+      localStorage.theme === "dark" ||
+      (!("theme" in localStorage) &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches)
+    ) {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, []);
+
   function handleClick() {
     document.documentElement.classList.toggle("dark");
   }
