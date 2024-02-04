42c0aaffd6b9eeb6f17315eaba1e90ca1208482d
Aimar Anand <aimaranand02@gmail.com>
2024-02-04 10:42:16 +0800

    fix(config): set configuration for mongoose etc.

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    serverComponentsExternalPackages: ["x-ray"],
+    esmExternals: "loose",
+    serverComponentsExternalPackages: ["mongoose"],
   },
+  //   webpack: (config) => {
+  //     config.experiments = {
+  //       topLevelAwait: true,
+  //     };
+  //     return config;
+  //   },
 };
 
 export default nextConfig;
