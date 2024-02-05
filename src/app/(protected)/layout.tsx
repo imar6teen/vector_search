734c8d7749571d6051b10c7680e26bd7a20931f3
Aimar Anand <aimaranand02@gmail.com>
2024-02-05 17:38:55 +0800

    fix(route): move protected route to folder called "(protected)"
    Authorization is work on protected folder, every user wants to access need to login first and it redirect to /api/auth/signin

---
+++ src/app/(protected)/layout.tsx
@@ -0,0 +1,14 @@
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+import { authOptions } from "../api/auth/[...nextauth]/route";
+
+export default async function ProtectedLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  const session = await getServerSession(authOptions);
+
+  if (!session) return redirect("/api/auth/signin");
+  return <>{children}</>;
+}
