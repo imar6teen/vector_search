92bc511497dd73c09d9b3fc100b2d31f769b5ec5
Aimar Anand <aimaranand02@gmail.com>
2024-02-07 22:02:15 +0800

    fix: add log in middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -9,6 +9,7 @@ import { withAuth } from "next-auth/middleware";
 export default withAuth(function middleware(req) {}, {
   callbacks: {
     authorized({ req, token }) {
+      console.log(token);
       if (
         (req.nextUrl.pathname.startsWith("/scrape") ||
           req.nextUrl.pathname.startsWith("/search")) &&
