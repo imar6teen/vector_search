0bea78ba03fdd16a9def7b416cc24230ca005727
Aimar Anand <aimaranand02@gmail.com>
2024-02-07 22:11:41 +0800

    fix: add secret at middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,6 +7,7 @@
 import { withAuth } from "next-auth/middleware";
 
 export default withAuth(function middleware(req) {}, {
+  secret: process.env.NEXTAUTH_SECRET,
   callbacks: {
     authorized({ req, token }) {
       console.log(token);
