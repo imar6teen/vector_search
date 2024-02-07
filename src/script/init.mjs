4cfeb33aff463ba040ac2061bb0cfb298f5335a2
Aimar Anand <aimaranand02@gmail.com>
2024-02-07 13:50:46 +0800

    fix(script): fix prebuild script

--- src/script/init.mjs
+++ src/script/init.mjs
@@ -6,6 +6,9 @@ import fs from "fs";
 import path from "path";
 
 (async () => {
+  const dir = path.join(process.cwd(), "keys");
+  if (!fs.existsSync(dir)) fs.mkdirSync(dir);
+
   const getMetadata = await head(process.env.BLOB_KEY_MONGO_URL, {
     token: process.env.BLOB_READ_WRITE_TOKEN,
   });
